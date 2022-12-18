6418699c2c3769c2e3119c9e8d893647b941b33c
Pablo Charamello <pablocharamello@gmail.com>
2022-12-18 00:48:10 -0300

    portal galaxy texture added

--- src/components/Portal.jsx
+++ src/components/Portal.jsx
@@ -1,11 +1,42 @@
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { useEffect } from "react";
-import { DoubleSide } from "three";
+import {
+  Scene,
+  WebGLRenderTarget,
+  TextureLoader,
+  EquirectangularReflectionMapping,
+  AlwaysStencilFunc,
+  ReplaceStencilOp,
+  DoubleSide,
+  LinearEncoding,
+} from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/gltfloader";
+import { FillQuad } from "./FillQuad";
+
+const scene = new Scene();
+scene.background = new TextureLoader().load(
+  "/textures/galaxy.jpg",
+  (texture) => {
+    texture.encoding = LinearEncoding;
+    texture.mapping = EquirectangularReflectionMapping;
+  }
+);
+
+const target = new WebGLRenderTarget(window.innerWidth, window.innerHeight);
+
+window.addEventListener("resize", () => {
+  target.setSize(window.innerWidth, window.innerHeight);
+});
 
 export function Portal() {
   const model = useLoader(GLTFLoader, "../../public/models/portal.glb");
   const mask = useLoader(GLTFLoader, "../../public/models/portal_mask.glb");
+
+  useFrame((state) => {
+    state.gl.setRenderTarget(target);
+    state.gl.render(scene, state.camera);
+    state.gl.setRenderTarget(null);
+  });
 
   useEffect(() => {
     if (!model) return;
@@ -15,12 +46,18 @@ export function Portal() {
 
     let maskMesh = mask.scene.children[0];
     maskMesh.material.side = DoubleSide;
+    maskMesh.material.transparent = false;
+    maskMesh.material.stencilWrite = true;
+    maskMesh.material.stencilRef = 1;
+    maskMesh.material.stencilFunc = AlwaysStencilFunc;
+    maskMesh.material.stencilZPass = ReplaceStencilOp;
   }, [model, mask]);
 
   return (
     <>
       <primitive object={model.scene} />
       <primitive object={mask.scene} />
+      <FillQuad map={target.texture} maskId={1} />
     </>
   );
 }
