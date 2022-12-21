c20694f4404869599c29bd959ebf4d01b7c40f36
Pablo Charamello <pablocharamello@gmail.com>
2022-12-21 12:08:04 -0300

    test 16

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
+    rollupOptions: {
+      external: [/^node:.*/],
+    },
     chunkSizeWarningLimit: 1600,
   },
 });
