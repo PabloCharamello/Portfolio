ce6c11c6f6557aa9a37244c0507259a7e0817551
Pablo Charamello <pablocharamello@gmail.com>
2022-12-20 21:56:12 -0300

    test 10

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,25 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  libs: [
+    {
+      libraryName: "GLTFLoader",
+      esModule: true,
+      resolveStyle: (FloatingIsland) =>
+        `/vercel/path0/src/components/FloatingIsland.jsx`,
+    },
+  ],
+
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
       // Add _all_ external dependencies here
-      external: isExternal,
+      external: ["GLTFLoader"],
+      output: {
+        globals: {
+          GLTFLoader: "GLTFLoader",
+        },
+      },
     },
   },
 });
