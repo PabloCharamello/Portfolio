61529c66f7253a1422a41015b6a53ceba94f389b
Pablo Charamello <pablocharamello@gmail.com>
2023-02-14 17:33:25 -0300

    lottie Wellcome removed and added h2 and anchor tags with contact links

--- src/components/ElementsPortfolio/lottieComponents/WellcomeAll.jsx
+++ src/components/ElementsPortfolio/lottieComponents/WellcomeAll.jsx
@@ -1,16 +1,37 @@
 import Lottie from "lottie-react";
-import wellcome from "../../../../public/lotties/wellcome.json";
+//import wellcome from "../../../../public/lotties/wellcome.json";
 import "../mainPage.css";
 
 const WellcomeAll = () => {
   return (
-    <div className="wellcome-all">
-      <Lottie
-        animationData={wellcome}
-        loop={true}
-        className="landingPageLottie"
-      />
-    </div>
+    <>
+      <div className="wellcome-all">
+        <h2
+          style={{ color: "#FFA500", fontSize: "3rem", margin: "-0px -20px" }}
+        >
+          Bienvenidos!
+        </h2>
+      </div>
+      <div className="contact-container">
+        <div className="contact-me">
+          <h2>Contáctame!</h2>
+        </div>
+        <a
+          className="links"
+          target={"_blank"}
+          href="https://www.linkedin.com/in/pablocharamello/"
+        >
+          Mi LinkedIn
+        </a>
+        <a
+          className="links"
+          target={"_blank"}
+          href="https://www.pablocharamello@gmail.com"
+        >
+          Mi correo
+        </a>
+      </div>
+    </>
   );
 };
 
